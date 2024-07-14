1366f16d7295b833fa78da715361247c84f621fc
Biniam Zerihun <127961220+b-iniona@users.noreply.github.com>
2024-07-14 18:34:57 +0300

    committed

---
+++ app/core/config.php
@@ -0,0 +1,4 @@
+<?php 
+
+define('ROOT', 'http://localhost/blog/public');
+define('APP_NAME', "My Blog");
---
+++ app/pages/contact.php
@@ -0,0 +1,7 @@
+   <?php include '../app/pages/includes/header.php'; ?>
+
+   <center>
+   <h4>Contact details:</h4>
+   Email: email@email.com
+   </center>
+    <?php include '../app/pages/includes/footer.php'; ?>
